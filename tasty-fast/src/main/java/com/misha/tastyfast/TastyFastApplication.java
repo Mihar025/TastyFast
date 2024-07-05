4f8692ce9cd5b1349bb9ffffaab7587b158d7efb
Mishar025 <mishamay583@gmail.com>
2024-07-05 13:32:35 -0400

    z

---
+++ tasty-fast/src/main/java/com/misha/tastyfast/TastyFastApplication.java
@@ -0,0 +1,13 @@
+package com.misha.tastyfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyFastApplication.class, args);
+    }
+
+}
