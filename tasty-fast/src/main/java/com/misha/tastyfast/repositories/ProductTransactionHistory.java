c217cf3479d21a40aba68b4aede60cca1777584d
Mishar025 <mishamay583@gmail.com>
2024-07-06 12:08:48 -0400

    z

---
+++ tasty-fast/src/main/java/com/misha/tastyfast/repositories/ProductTransactionHistory.java
@@ -0,0 +1,30 @@
+package com.misha.tastyfast.repositories;
+
+import com.misha.tastyfast.comon.BaseEntity;
+import com.misha.tastyfast.model.Product;
+import com.misha.tastyfast.model.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductTransactionHistory extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
