884374823b50c789cf79a6230b72377eb4422d8f
Mishar025 <mishamay583@gmail.com>
2024-07-07 16:42:49 -0400

    z

---
+++ tasty-fast/src/main/java/com/misha/tastyfast/requests/DishesRequest.java
@@ -0,0 +1,25 @@
+package com.misha.tastyfast.requests;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record DishesRequest (
+        Integer id,
+        @NotNull(message = "100")
+        @NotEmpty(message = "100")
+        String  dishesName,
+        @NotNull(message = "101")
+        @NotEmpty(message = "101")
+        String dishesDescription,
+        @NotNull(message = "102")
+        @NotEmpty(message = "102")
+        double price,
+        @NotNull(message = "103")
+        @NotEmpty(message = "103")
+        BigDecimal calories,
+
+        boolean inStock
+){
+}
