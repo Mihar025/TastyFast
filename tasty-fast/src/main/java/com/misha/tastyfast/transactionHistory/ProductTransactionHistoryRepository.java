4a2e8e3ff572231f096ddd2a4dde88cffb0d9dca
Mishar025 <mishamay583@gmail.com>
2024-07-09 19:17:15 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/transactionHistory/ProductTransactionHistoryRepository.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/transactionHistory/ProductTransactionHistoryRepository.java
@@ -1,13 +1,13 @@
-package com.misha.tastyfast.repositories;
+package com.misha.tastyfast.transactionHistory;
 
-import com.misha.tastyfast.repositories.ProductTransactionHistory;
+import com.misha.tastyfast.transactionHistory.ProductTransactionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProducTransactionHistoryRepository extends JpaRepository<ProductTransactionHistory, Integer> {
+public interface ProductTransactionHistoryRepository extends JpaRepository<ProductTransactionHistory, Integer> {
     @Query(
             """
             select 
@@ -15,7 +15,7 @@ public interface ProducTransactionHistoryRepository extends JpaRepository<Produc
             FROM ProductTransactionHistory  productTransactionHistory
             WHERE productTransactionHistory.user.id = :userId
             AND productTransactionHistory.product.id= :productId
-            AND productTransactionHistory.alreadyOrdered = false 
+           
     """
     )
     boolean isAlreadyOrderedByUser(@Param("productId") Integer productId, @Param("userId") Integer userId);
