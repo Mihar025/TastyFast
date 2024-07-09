9adb46019498c8e8b410bc8c2c9a2384430af41e
Mishar025 <mishamay583@gmail.com>
2024-07-08 20:26:55 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/model/Feedback.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/model/Feedback.java
@@ -17,14 +17,30 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Feedback extends BaseEntity {
+    private double note;
+    private String comment;
 
-
-    private double note; // 1-5 stars
-    private String comment; // feed back about book or whatever
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
+    @ManyToOne
+    @JoinColumn(name = "dishes_id")
+    private Dishes dishes;
 
+    @ManyToOne
+    @JoinColumn(name = "drink_id")
+    private Drink drink;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
 }
