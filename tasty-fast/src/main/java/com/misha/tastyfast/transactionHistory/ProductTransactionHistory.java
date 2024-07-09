21fbebff09c1ae7fecc257318532da2ecd81c4d5
Mishar025 <mishamay583@gmail.com>
2024-07-09 19:17:09 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/transactionHistory/ProductTransactionHistory.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/transactionHistory/ProductTransactionHistory.java
@@ -1,7 +1,8 @@
-package com.misha.tastyfast.repositories;
+package com.misha.tastyfast.transactionHistory;
 
 import com.misha.tastyfast.comon.BaseEntity;
 import com.misha.tastyfast.model.Product;
+import com.misha.tastyfast.model.Store;
 import com.misha.tastyfast.model.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
@@ -11,6 +12,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -23,11 +27,17 @@ public class ProductTransactionHistory extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
-    private boolean returned;
-    private boolean alreadyOrdered;
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
 
+    private String transactionType;
+    private LocalDateTime transactionDate;
+    private BigDecimal amount;
+    private String details;
 }
