03d218869c2b54ac39d573202afe3b0cf31cf301
Mishar025 <mishamay583@gmail.com>
2024-07-11 18:16:45 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/config/BeansConfig.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/config/BeansConfig.java
@@ -1,11 +1,14 @@
 package com.misha.tastyfast.config;
 
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -17,10 +20,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @RequiredArgsConstructor
@@ -54,46 +55,38 @@ public class BeansConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-
-        cacheManager.setCacheSpecification(
-                "store:byId: maximumSize=500, expireAfterWrite=60m; " +
-                        "store:allProducts: maximumSize=100, expireAfterWrite=30m; " +
-                        "store:product:update: maximumSize=200, expireAfterWrite=15m; " +
-                        "store:drink:byId: maximumSize=300, expireAfterWrite=20m; " +
-                        "store:allDrinks: maximumSize=200, expireAfterWrite=30m; " +
-                        "store:drinks:update: maximumSize=300, expireAfterWrite=30m; " +
-                        "store:update: maximumSize=200, expireAfterWrite=15m; " +
-                        "store:allStores: maximumSize=200, expireAfterWrite=15m; " +
-                        "store:allStoresWithoutDelivery: maximumSize=200, expireAfterWrite=15m; " +
-
-                        "restaurant:byId: maximumSize=300, expireAfterWrite=20m; " +
-                         "restaurant:allRestaurants: maximumSize=500, expireAfterWrite=30m; " +
-                        "restaurant:dish:byId: maximumSize=600, expireAfterWrite=30m; " +
-                        "restaurant:allDish: maximumSize=600, expireAfterWrite=30m; " +
-                        "restaurant:dish:update: maximumSize=300, expireAfterWrite=30m; " +
-
-                        "restaurant:drink:byId: maximumSize=300, expireAfterWrite=20m; " +
-                       "restaurant:allDrinks: maximumSize=200, expireAfterWrite=30m; " +
-                        "restaurant:drinks:update: maximumSize=300, expireAfterWrite=30m; " +
-                        "restaurant:allRestaurantsNoDelivery: maximumSize=300, expireAfterWrite=30m"
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        List<CaffeineCache> caches = Arrays.asList(
+                buildCache("store_byId", 500, 60),
+                buildCache("store_allProducts", 100, 30),
+                buildCache("store_product_update", 200, 15),
+                buildCache("store_drink_byId", 300, 20),
+                buildCache("store_allDrinks", 200, 30),
+                buildCache("store_drinks_update", 300, 30),
+                buildCache("store_update", 200, 15),
+                buildCache("store_allStores", 200, 15),
+                buildCache("store_allStoresWithoutDelivery", 200, 15),
+                buildCache("restaurant_byId", 300, 20),
+                buildCache("restaurant_allRestaurants", 500, 30),
+                buildCache("restaurant_dish_byId", 600, 30),
+                buildCache("restaurant_allDish", 600, 30),
+                buildCache("restaurant_dish_update", 300, 30),
+                buildCache("restaurant_drink_byId", 300, 20),
+                buildCache("restaurant_allDrinks", 200, 30),
+                buildCache("restaurant_drinks_update", 300, 30),
+                buildCache("restaurant_allRestaurantsNoDelivery", 300, 30)
         );
 
-        List<String> cacheNames = new ArrayList<>(Arrays.asList(
-                "store:byId", "store:allProducts", "store:product:update",
-                "store:drink:byId", "store:allDrinks", "store:drinks:update","store:allStores", "store:store:byId","store:update",
-                "restaurant:byId", "restaurant:allRestaurants", "restaurant:dish:byId",
-                "restaurant:allDish", "restaurant:dish:update",
-                "restaurant:drink:byId", "restaurant:allDrinks", "restaurant:drinks:update", "restaurant:allRestaurantsNoDelivery"
-        ));
-        cacheManager.setCacheNames(cacheNames);
-
+        cacheManager.setCaches(caches);
         return cacheManager;
     }
 
-
-
-
+    private CaffeineCache buildCache(String name, int maxSize, int expireAfter) {
+        return new CaffeineCache(name, Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireAfter, TimeUnit.MINUTES)
+                .build());
+    }
 
 
 
