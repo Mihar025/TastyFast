e122c1ed78392e5d3df168b16d0a8afaa571643f
Mishar025 <mishamay583@gmail.com>
2024-07-13 16:43:54 -0400

    z

---
+++ tasty-fast/src/main/java/com/misha/tastyfast/Controllers/OrderController.java
@@ -0,0 +1,40 @@
+package com.misha.tastyfast.Controllers;
+
+import com.misha.tastyfast.model.Order;
+import com.misha.tastyfast.requests.OrderRequest;
+import com.misha.tastyfast.requests.OrderResponse;
+import com.misha.tastyfast.requests.PageResponse;
+import com.misha.tastyfast.services.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody OrderRequest orderRequest, Authentication authentication) {
+        Order createdOrder = orderService.createOrder(orderRequest, authentication);
+        return ResponseEntity.ok(createdOrder);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Integer orderId, Authentication authentication) {
+        Order order = orderService.getOrderById(orderId, authentication);
+        return ResponseEntity.ok(order);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<OrderResponse>> getUserOrders(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            Authentication authentication) {
+        PageResponse<OrderResponse> response = orderService.getUserOrders(page, size, authentication);
+        return ResponseEntity.ok(response);
+    }
+}
