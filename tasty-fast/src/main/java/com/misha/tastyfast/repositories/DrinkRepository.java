e4f850081adb0673e047483b2616c31a489447aa
Mishar025 <mishamay583@gmail.com>
2024-07-13 16:43:19 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/repositories/DrinkRepository.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/repositories/DrinkRepository.java
@@ -1,5 +1,6 @@
 package com.misha.tastyfast.repositories;
 
+import com.misha.tastyfast.model.Dishes;
 import com.misha.tastyfast.model.Drink;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,28 +25,14 @@ public interface DrinkRepository extends JpaRepository<Drink, Integer>, JpaSpeci
             )
     Page<Drink> findAllDisplayableDrinks(Pageable pageable, Integer id);
 
-    @Query(
-            """ 
-        SELECT drinks
-        FROM Drink drinks
-        WHERE drinks.restaurant.id =: restaurantId
-    """
-    )
+    @Query("SELECT d FROM Drink d WHERE d.restaurant.id = :restaurantId")
     Page<Drink> findAllDrinksInRestaurant(Pageable pageable, @Param("restaurantId") Integer restaurantId);
 
 
     Optional<Drink> findByIdAndRestaurantId(Integer drinkId, Integer restaurantId);
 
-    @Query(
-
-            """
-            select drinks
-            from Drink  drinks
-            WHERE drinks.store.id=: storeId
-            """
-    )
-    Page<Drink> findAllDrinksInStore(Pageable pageable, Integer storeId);
-
+    @Query("SELECT d FROM Drink d WHERE d.store.id = :storeId")
+    Page<Drink> findAllDrinksInStore(Pageable pageable, @Param("storeId") Integer storeId);
 
 
     Optional<Drink> findByIdAndStoreId(Integer drinkId, Integer storeId);
