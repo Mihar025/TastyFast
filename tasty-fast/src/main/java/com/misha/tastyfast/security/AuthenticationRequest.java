59e4bde8777adef4dd9fa15eb0263184025af8eb
Mishar025 <mishamay583@gmail.com>
2024-07-15 20:38:23 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/security/AuthenticationRequest.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/security/AuthenticationRequest.java
@@ -7,16 +7,15 @@ import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.SpringBootVersion;
 
 @Getter
 @Setter
 @Builder
 public class AuthenticationRequest {
 
-    @Email(message = "Email is not well formatted! \n Example user123@mail.com")
-    @NotEmpty(message = "Email field cannot be empty")
-    @NotNull(message = "Email is mandatory")
+    @Email(message = "EmailRequest is not well formatted! \n Example user123@mail.com")
+    @NotEmpty(message = "EmailRequest field cannot be empty")
+    @NotNull(message = "EmailRequest is mandatory")
     private String email;
     @NotEmpty(message = "Password field cannot be empty")
     @NotNull(message = "Password cannot be null")
