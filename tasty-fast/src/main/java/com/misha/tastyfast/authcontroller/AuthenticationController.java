691590a00e0481bf87969e236db3a6b6ad17fe66
Mishar025 <mishamay583@gmail.com>
2024-07-15 20:38:18 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/authcontroller/AuthenticationController.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/authcontroller/AuthenticationController.java
@@ -1,6 +1,6 @@
 package com.misha.tastyfast.authcontroller;
 
-import com.misha.tastyfast.requests.RegistrationRequest;
+import com.misha.tastyfast.requests.registrationRequests.RegistrationRequest;
 import com.misha.tastyfast.security.AuthenticationRequest;
 import com.misha.tastyfast.security.AuthenticationResponse;
 import com.misha.tastyfast.security.AuthenticationService;
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,10 +22,23 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
-            authenticationService.register(request);
-            return ResponseEntity.accepted().build();
+
+    public ResponseEntity<?> registerUser(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
+            authenticationService.registerUser(request);
+            return ResponseEntity.ok("User register successfully");
+    }
+
+    @PostMapping("/register/business")
+    public ResponseEntity<?> registerBusinessAccount(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
+        authenticationService.registerBusinessAccount(request);
+        return ResponseEntity.ok("User register successfully");
+    }
+
+    @PostMapping("/register/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> registerAdmin(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
+        authenticationService.registerAdmin(request);
+        return ResponseEntity.ok("User register successfully");
     }
 
     @PostMapping("/authenticate")
