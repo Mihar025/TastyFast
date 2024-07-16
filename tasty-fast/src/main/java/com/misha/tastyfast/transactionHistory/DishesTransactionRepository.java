57d2c5194adaeec2c041a1d8e06d9e30cc6ab3c3
Mishar025 <mishamay583@gmail.com>
2024-07-16 19:54:02 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/transactionHistory/DishesTransactionRepository.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/transactionHistory/DishesTransactionRepository.java
@@ -1,6 +1,5 @@
 package com.misha.tastyfast.transactionHistory;
 
-import com.misha.tastyfast.transactionHistory.DishesTransactionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
