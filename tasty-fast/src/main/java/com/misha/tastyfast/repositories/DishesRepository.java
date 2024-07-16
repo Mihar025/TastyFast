61e61c1d51e9bb6e08a84f9d6f787369d6803833
Mishar025 <mishamay583@gmail.com>
2024-07-15 20:39:40 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/repositories/DishesRepository.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/repositories/DishesRepository.java
@@ -1,10 +1,8 @@
 package com.misha.tastyfast.repositories;
 
 import com.misha.tastyfast.model.Dishes;
-import com.misha.tastyfast.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
