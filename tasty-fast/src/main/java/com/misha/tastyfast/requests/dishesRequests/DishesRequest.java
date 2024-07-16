827df30db158fdd11d0728ea04ee967bae51ff8f
Mishar025 <mishamay583@gmail.com>
2024-07-16 19:53:21 -0400

    z

--- tasty-fast/src/main/java/com/misha/tastyfast/requests/dishesRequests/DishesRequest.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/requests/dishesRequests/DishesRequest.java
@@ -1,4 +1,4 @@
-package com.misha.tastyfast.requests;
+package com.misha.tastyfast.requests.dishesRequests;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
